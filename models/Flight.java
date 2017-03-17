9e8e84bda152dc0b1f81e98c7a74e234cd698e07
gudrunthora <gudrunthora@yahoo.com>
2017-03-17 14:29:32 +0000

    Flight class added

--- models/Flight.java
+++ models/Flight.java
@@ -1,5 +1,76 @@
 package models;
 
-public class Flight {
+//nota kannski Date fyrir dagsetningarnar?
+//import java.util.Date;
 
+public class Flight {
+	private int flightID;
+	private String flightDepart;
+	private String flightDest;
+	private String flightTime; //  hh:mm 		kannski nota Date frekar?
+	private String flightDate; //  dd/mm/yyyy 	kannski nota Date frekar?
+	private int availableSeats;
+	private int flightPrice;
+	private final int MAX_SEATS = 50;
+	
+	// Costructor
+	public Flight(int flightID, String flightDepart, String flightDest, String flightTime, String flightDate, int availableSeats, int flightPrice) {
+		this.flightID = flightID;
+		this.flightDepart = flightDepart;
+		this.flightDest = flightDest;
+		this.flightTime = flightTime;
+		this.flightDate = flightDate;
+		this.availableSeats = availableSeats;
+		this.flightPrice = flightPrice;
+	}
+	
+	
+//Ekki víst að það þurfi þessi föll
+	
+//	public void bookSeats(int nr) {
+//		if (this.availableSeats-nr >= 0) {
+//			this.availableSeats = this.availableSeats - nr;
+//		} else {
+//			//senda eitthvað error?
+//		}
+//	}
+//	
+//	public void cancelSeats(int nr) {
+//		if (this.availableSeats+nr <= this.MAX_SEATS) {
+//			this.availableSeats = this.availableSeats + nr;
+//		} else {
+//			//senda eitthvað error?
+//		}
+//	}
+	
+	public int getFlightID() {
+		return this.flightID;
+	}
+	
+	public String getFlightDepart() {
+		return this.flightDepart;
+	}
+	
+	public String getFlightDest() {
+		return this.flightDest;
+	}
+	
+	public String getFlightTime() {
+		return this.flightTime;
+	}
+	
+	public String getFlightDate() {
+		return this.flightDate;
+	}
+	
+	public int getAvailableSeats() {
+		return this.availableSeats;
+	}
+	
+	public int getFlightPrice() {
+		return this.flightPrice;
+	}
 }
+
+
+

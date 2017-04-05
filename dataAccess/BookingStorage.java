ab778f9e26cad039e85e029c9a5a7f5991b08b53
gudrunthora <gudrunthora@yahoo.com>
2017-04-05 11:32:24 +0000

    Worked on Booking storage

--- dataAccess/BookingStorage.java
+++ dataAccess/BookingStorage.java
@@ -1,17 +1,59 @@
 package dataAccess;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 import models.*;
 
 public class BookingStorage {
 
 
-	public void addBooking(Booking b) {
+	public int addBooking(Booking b) {
 	// Add the booking to the database
-		
+		DatabaseManager dm = new DatabaseManager();
+		dm.connect();
+		String sql = "INSERT INTO Bookings(flight, nrAdult, nrChildren, nrBag, totalPrice, specialNeeds)" +
+		"VALUES ("+b.getFlightID()+", "+b.getNrAdult()+", "+b.getNrChildren()+", "+b.getNrBag()+", "+b.getTotalPrice()+", '"+b.getSpecialNeeds()+"');";
+		System.out.println(sql);
+		int id = dm.updateDatabaseGetID(sql);
+		System.out.println(id);
+		dm.disconnect();
 	
-		//Minnka fjölda lausra sæta í vélinni um fjölda farþega í bókun
+	// Reserve seats for the passengers in the chosen flight
+		updateAvailableSeats(b.getFlightID(), -(b.getNrAdult()+b.getNrChildren()));
+		
+		return id;
 	}
 	
-	public void removeBooking(Booking b) {
+	// returns true if there is some booking with this id in the database and false if not
+	public boolean isInDatabase(int id) {
+		// Connect to the database
+		DatabaseManager dm = new DatabaseManager();
+		dm.connect();
+				
+		// Query the database
+		String sql = "SELECT * FROM Bookings Where bookingID = '" + id +";";
+				
+		ResultSet rs = dm.queryDatabase(sql);
+		System.out.println(sql);
+				
+		// Check if there is a booking with the id
+		boolean exists = false;
+		try {
+			if (rs.next() ) {
+				exists = true;
+			} else {
+				exists = false;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		dm.disconnect();
+		
+		return exists;
+	}
+	
+	public void removeBooking(int id) {
 		//ToDo:
 		//Eyða þessari bókun úr grunni
 		//Auka fjölda lausra sæta í vélinni um fjölda farþega í bókun
@@ -35,19 +77,39 @@ public class BookingStorage {
 	// Create the Bookings table in database
 //		DatabaseManager dm = new DatabaseManager();
 //		dm.connect();
-//		String sql = "CREATE TABLE Bookings " +
-//				"(bookingID INTEGER NOT NULL, " +
-//				"flight INTEGER, " + 
+//		String sql = "CREATE TABLE Bookings ( " +
+//				"bookingID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+//				"flight INTEGER NOT NULL, " + 
 //				"nrAdult INTEGER, " +
 //				"nrChildren INTEGER, " +
 //				"nrBag INTEGER, " +
-//				"totalPrice TEXT, " +
+//				"totalPrice INTEGER, " +
 //				"specialNeeds TEXT, " + 
-//				"PRIMARY KEY ( bookingID ),"+
-//				"FOREIGN KEY (flight) REFERENCES Flights (flightID));";
+//				"FOREIGN KEY (flight) REFERENCES Flights(flightID));";
 //		System.out.println(sql);
 //		dm.updateDatabase(sql);
 //		dm.disconnect();
+		
+
+		// Add a booking to the table
+//		DatabaseManager dm = new DatabaseManager();
+//		dm.connect();
+//		String sql = "INSERT INTO Bookings(flight, nrAdult, nrChildren, nrBag, totalPrice, specialNeeds)" +
+//		"VALUES (313, 2, 1, 3, 35000, 'Peanut allergies!');";
+//		System.out.println(sql);
+//		dm.updateDatabase(sql);
+//		dm.disconnect();
+		
+		
+//		DatabaseManager dm = new DatabaseManager();
+//		dm.connect();
+//		String sql = "INSERT INTO Bookings(flight, nrAdult, nrChildren, nrBag, totalPrice, specialNeeds)" +
+//		"VALUES (313, 2, 1, 3, 35000, 'Peanut allergies!');";
+//		System.out.println(sql);
+//		int id = dm.updateDatabaseGetID(sql);
+//		System.out.println(id);
+//		dm.disconnect();
+
 	}
 	
 }
---
+++ test/BookingStorageClassTest.java
@@ -0,0 +1,67 @@
+package test;
+import models.*;
+import managers.*;
+import dataAccess.*;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import managers.PassengerManager;
+
+public class BookingStorageClassTest {
+	public BookingStorage bs;
+	public PassengerManager pm;
+	public Flight flight;
+	public int nrBag;
+	public Passenger p_a, p_b, p_c;
+	public ArrayList<Passenger> passengers;
+	public String specialNeeds;
+	public Booking b;
+	public int id;
+	
+
+	@Before
+	public void setUp() throws Exception {
+		bs = new BookingStorage();
+		flight = new Flight(201, "GRM", "AEY", "15:00", "17/04/2017", 35, 17000);
+		nrBag = 6; 
+		p_a = new Passenger("Jón", true);
+		p_b = new Passenger("Gunna", true);
+		p_c = new Passenger("Óli", false);
+		pm.addPassenger(p_a);
+		pm.addPassenger(p_b);
+		pm.addPassenger(p_c);
+		passengers = pm.getPassengers();
+		specialNeeds = "We are travelling with a dog";
+		b = new Booking(flight, nrBag, passengers, specialNeeds);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		bs = null;
+		flight = null;
+		p_a = null;
+		p_b = null;
+		p_c = null;
+		passengers = null;
+		specialNeeds = null;
+		b = null;
+	}
+
+	@Test
+	public void testIfAddedToDatabase() {
+		id = bs.addBooking(b);
+		assertTrue(bs.isInDatabase(id));
+	}
+	
+	@Test
+	public void testIfDeletedFromDatabase() {
+		bs.removeBooking(id);
+		assertFalse(bs.isInDatabase(id));
+	}
+
+}
